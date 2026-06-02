a8de5ee755301cc7a44d2d1bef77ea829f388916
Auez <kemelove@bk.ru>
2026-06-02 17:33:53 +0500

    fix bag

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useSearchParams } from "next/navigation";
 import AuthLayout from "@/components/auth/AuthLayout";
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
