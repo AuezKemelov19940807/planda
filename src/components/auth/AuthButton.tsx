eac0ded2191c115d116a4e900d5174bbb47550c9
Auez <kemelove@bk.ru>
2026-06-02 17:54:30 +0500

    fix bag

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function AuthButton({ text, loading }: Props) {
   return (
     <button
-      className="text-sm font-bold text-white bg-green py-1.5 rounded-lg cursor-pointer  h-12 flex items-center justify-center"
+      className="text-sm font-bold text-white bg-green py-1.5 rounded-lg cursor-pointer  px-5 h-12 flex items-center justify-center"
       type="submit"
       disabled={loading}
     >
