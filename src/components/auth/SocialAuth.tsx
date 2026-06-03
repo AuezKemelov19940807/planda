4cef27953f1ba805fe271ea44a52c050bd0b659b
Auez <kemelove@bk.ru>
2026-06-03 10:32:06 +0500

    change link auth/google

--- src/components/auth/SocialAuth.tsx
+++ src/components/auth/SocialAuth.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 export default function SocialAuth() {
+  const API_URL = "https://planola-back.fly.dev";
+
   const handleGoogle = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`;
+    window.location.href = `${API_URL}/auth/google`;
   };
 
   return (
     <div className="grid grid-cols-2 gap-x-6 items-center mt-22">
-      <button className="flex items-center gap-x-2.5 cursor-pointer border-gray-300 border rounded-lg py-1 px-6 text-xs font-medium cursor-pointer" onClick={handleGoogle}  >
+      <button
+        className="flex items-center gap-x-2.5  border-gray-300 border rounded-lg py-1 px-6 text-xs font-medium cursor-pointer"
+        onClick={handleGoogle}
+      >
         <span>
           <img src="/icons/google.svg" alt="Google Icon" />
         </span>
